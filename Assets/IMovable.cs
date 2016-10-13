004d54d28880322bde1501f146f45430f30b3d3a
Justin <jlehmann96@gmail.com>
2016-10-13 11:48:40 -0600

    Added Interfaces for Damageable and Movement. Started configuring Inputs.

---
+++ Assets/IMovable.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IMovable {
+	void MoveUp(float velocity);
+	void MoveDown(float velocity);
+	void MoveLeft(float velocity);
+	void MoveRight(float velocity);
+}
