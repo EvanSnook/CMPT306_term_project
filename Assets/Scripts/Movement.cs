3e2630097dbacfadc10aea7decf6b09cffb53f36
Justin <jlehmann96@gmail.com>
2016-10-13 15:23:57 -0600

    Added basic movement code. Abandoned old Interface method of movement. Added placeholder sprite

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	private Rigidbody2D rigidBody;
+
+	void Start () {
+		 rigidBody = GetComponent<Rigidbody2D>();
+	}
+
+	public void MoveUp (float velocity) {
+		rigidBody.velocity += Vector2.up * velocity;
+	}
+
+	public void MoveDown (float velocity) {
+		rigidBody.velocity += Vector2.down * velocity;
+	}
+
+	public void MoveLeft (float velocity) {
+		rigidBody.velocity += Vector2.left * velocity;
+	}
+
+	public void MoveRight (float velocity) {
+		rigidBody.velocity += Vector2.right * velocity;
+	}
+
+	/*
+	SlowMovement can be used to slow horizontal movement of a rigidBody
+	Effectively the same as drag except solely on the x axis
+	*/
+	public void SlowMovement () {
+		rigidBody.velocity = new Vector2(rigidBody.velocity.x*0.5f, rigidBody.velocity.y);
+	}
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void FixedUpdate () {
+		if (Input.GetAxisRaw("Horizontal") < -0.1) {
+			this.SendMessage("MoveLeft", 5.0);
+		} else if (Input.GetAxisRaw("Horizontal") > 0.1) {
+			this.SendMessage("MoveRight", 5.0);
+		}
+		this.SendMessage("SlowMovement");
+	}
+}
