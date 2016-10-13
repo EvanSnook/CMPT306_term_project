83c35b5927a405fc1d05170fd164f63905889e9f
Tim <XxWolf9630xX@gmail.com>
2016-10-13 14:11:29 -0600

    Started Script for projectile creating and movement

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+	/*
+	1. Create prjectile.
+		
+	2. Launch with force in direction of mouse
+	3. If hit tag of platform/player/boss
+	4. 
+
+	 */
+
+	public Rigidbody projectile;
+	public float speed = 10f;
+
+	void FireProjectile () {
+		Rigidbody projectileClone = (Rigidbody) Instantiate(projectile, transform.position, transform.rotation);
+		projectileClone.velocity = transform.forward * speed;
+
+		// You can also acccess other components / scripts of the clone
+		//		rocketClone.GetComponent<MyRocketScript>().DoSomething();
+	}
+
+	// Calls the fire method when holding down ctrl or mouse
+	void Update () {
+		if (Input.GetButtonDown("Fire1")) {
+			FireProjectile();
+		}
+	}
+}
