e1cefe1be29c16a33d23ce118d34ca73235c9910
Tim <XxWolf9630xX@gmail.com>
2016-10-13 22:12:22 -0600

    Made projectile be created at mouse position. Need to make projectile fire at mouse position not spawn there. Removed I believe all the excess files that we did not need.

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -3,30 +3,25 @@ using System.Collections;
 
 public class Projectile : MonoBehaviour {
 
-	/*
-	1. Create prjectile.
-		
-	2. Launch with force in direction of mouse
-	3. If hit tag of platform/player/boss
-	4. 
+	public GameObject projectilePrefab; // This is the projectile that will be fired.
+	public float speed = 10f; // This is the speed of the projectile.
+	public GameObject clone; // This is the cloned Object.
 
+	/* This method creates a projectile and fires it at the mouse Position.
+	 * 
+	 * Current Problem: Only creates object at mouse position, does not fire projectile at mouse position.
 	 */
+	void FireProjectileAtMouse () {
+		Vector3 MouseInWorld = Camera.main.ScreenToWorldPoint (Input.mousePosition); // This get the position of the mouse in the worlds coordinates.
 
-	public Rigidbody projectile;
-	public float speed = 10f;
-
-	void FireProjectile () {
-		Rigidbody projectileClone = (Rigidbody) Instantiate(projectile, transform.position, transform.rotation);
-		projectileClone.velocity = transform.forward * speed;
-
-		// You can also acccess other components / scripts of the clone
-		//		rocketClone.GetComponent<MyRocketScript>().DoSomething();
+		Instantiate(projectilePrefab, MouseInWorld, Quaternion.Euler(MouseInWorld)); // Creates ProjectilePrefab at mouse position. 
 	}
+
 
 	// Calls the fire method when holding down ctrl or mouse
 	void Update () {
 		if (Input.GetButtonDown("Fire1")) {
-			FireProjectile();
+			FireProjectileAtMouse();
 		}
 	}
 }
