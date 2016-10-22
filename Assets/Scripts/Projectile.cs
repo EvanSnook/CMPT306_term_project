bd369c5de0d0c7c2f1b9c65d82b490dee88432d5
Tim <XxWolf9630xX@gmail.com>
2016-10-22 10:37:38 -0600

    Finished Making a Script that will launch a Projectile towards the Mouse.

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -2,26 +2,44 @@
 using System.Collections;
 
 public class Projectile : MonoBehaviour {
+	public float ProjectileSpeed; // Movement Speed of the projectile.
+	public GameObject ProjectilePrefab; // The Projectile that will be fired.
+	private GameObject Clone; // The fired Projectile.
 
-	public GameObject projectilePrefab; // This is the projectile that will be fired.
-	public float speed = 10f; // This is the speed of the projectile.
-	public GameObject clone; // This is the cloned Object.
+	public float CooldownTimer; // How long until can fire again.
+	public float DestroyProjectileAfter; // How long until the shot projectile is automatically destroyed.
 
-	/* This method creates a projectile and fires it at the mouse Position.
-	 * 
-	 * Current Problem: Only creates object at mouse position, does not fire projectile at mouse position.
-	 */
-	void FireProjectileAtMouse () {
-		Vector3 MouseInWorld = Camera.main.ScreenToWorldPoint (Input.mousePosition); // This get the position of the mouse in the worlds coordinates.
-
-		Instantiate(projectilePrefab, MouseInWorld, Quaternion.Euler(MouseInWorld)); // Creates ProjectilePrefab at mouse position. 
-	}
+	private float Timer; // This is the the clock that keeps track of how much of a cooldown is left.
+	private bool CanFire; // This keeps track of if you can fire or not.
 
 
 	// Calls the fire method when holding down ctrl or mouse
-	void Update () {
-		if (Input.GetButtonDown("Fire1")) {
+	void FixedUpdate () {
+		if (Timer <= 0) { // This Checks that the Cooldown Time has passed.
+			CanFire = true;
+		} else {
+			Timer -= Time.deltaTime; // If the Cooldown Time hasn't Passed Keep counting down.
+		}
+				
+		if (Input.GetButtonDown("Fire1") && CanFire) { // If Fire1 pressed and CanFire then FireProjectileAtMouse.
+			CanFire = false;
 			FireProjectileAtMouse();
+			Timer = CooldownTimer; // Set Cooldown Timer.
 		}
 	}
+
+	/*
+		This Finds the Mouse and Fires a Projectile in the Mouses Direction.
+	 */
+	private void FireProjectileAtMouse() {
+		Vector3 MousePosition = Input.mousePosition; // Get the Mouse Position.
+		MousePosition.z = transform.position.z - Camera.main.transform.position.z;
+		MousePosition = Camera.main.ScreenToWorldPoint(MousePosition);
+
+		Quaternion AngleToMouse = Quaternion.FromToRotation(Vector3.up, MousePosition - transform.position);
+		Clone = Instantiate(ProjectilePrefab, transform.position, AngleToMouse) as GameObject;
+		Clone.GetComponent<Rigidbody2D>().AddForce(Clone.transform.up * ProjectileSpeed); // Launch Projectile forward to Mouse.
+		Destroy (Clone, DestroyProjectileAfter); // Destroy Projectile after a certain time.
+	}
+
 }
