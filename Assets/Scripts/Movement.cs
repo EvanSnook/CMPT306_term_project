94dbe1e2068ec8c34b6355ac5fef9165701de588
Tim <XxWolf9630xX@gmail.com>
2016-10-27 14:03:15 -0600

    Added Some comments and Changed it so that from the Player Controller Script instead of sending movement speed from there to the Movement script, the Movement script holds the variable for movement speed.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,25 +4,20 @@ using System.Collections;
 public class Movement : MonoBehaviour {
 
 	private Rigidbody2D rigidBody;
+	public float LeftRightMovementSpeed; // This is how fast movement left and right is.
+	public float JumpSpeed; // This is how fast the jump is.
+
 
 	void Start () {
 		 rigidBody = GetComponent<Rigidbody2D>();
 	}
 
-	public void MoveUp (float velocity) {
-		rigidBody.velocity += Vector2.up * velocity;
+	public void MoveLeft () {
+		rigidBody.velocity += Vector2.left * LeftRightMovementSpeed;
 	}
 
-	public void MoveDown (float velocity) {
-		rigidBody.velocity += Vector2.down * velocity;
-	}
-
-	public void MoveLeft (float velocity) {
-		rigidBody.velocity += Vector2.left * velocity;
-	}
-
-	public void MoveRight (float velocity) {
-		rigidBody.velocity += Vector2.right * velocity;
+	public void MoveRight () {
+		rigidBody.velocity += Vector2.right * LeftRightMovementSpeed;
 	}
 
 	/*
@@ -31,6 +26,11 @@ public class Movement : MonoBehaviour {
 	*/
 	public void SlowMovement () {
 		rigidBody.velocity = new Vector2(rigidBody.velocity.x*0.5f, rigidBody.velocity.y);
+	}
+
+
+	public void Jump() { // Needs to no be able to jump when not on ground.
+		rigidBody.velocity += Vector2.up * JumpSpeed;
 	}
 
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,22 +3,16 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-
-	}
-
-	// Update is called once per frame
-	void Update () {
-
-	}
-
 	void FixedUpdate () {
-		if (Input.GetAxisRaw("Horizontal") < -0.1) {
-			this.SendMessage("MoveLeft", 5.0);
-		} else if (Input.GetAxisRaw("Horizontal") > 0.1) {
-			this.SendMessage("MoveRight", 5.0);
+		if (Input.GetAxisRaw ("Horizontal") < -0.1) { // This get's the Horizontal input and checks to see if you are pushing the left or right key's.
+			this.SendMessage ("MoveLeft");
+		} else if (Input.GetAxisRaw ("Horizontal") > 0.1) {
+			this.SendMessage ("MoveRight");
 		}
-		this.SendMessage("SlowMovement");
+		this.SendMessage("SlowMovement"); // If you aren't going left or right do SlowMovement.
+
+		if (Input.GetAxisRaw("Jump") > 0.1) { // This get's the input for the jump and sends message to jump if pushed.
+			this.SendMessage ("Jump");
+		}
 	}
 }
