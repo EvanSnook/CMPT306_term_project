0c047c181fdd3beb7f880c985cca870250fa0280
Tim <XxWolf9630xX@gmail.com>
2016-11-02 16:36:30 -0600

    Created GUI and Some elements. Started Script that will all those elements to be updated when something happens.

---
+++ Assets/Scripts/PlayerGUIManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerGUIManager : MonoBehaviour {
+
+	public GameObject MainGUI;
+
+	Text HealthNumberGUI; // This is the Element that Holds how much Health the player has.
+	Text DeathsNumberGUI; // This is the Element that Holds how many times the player has died.
+
+
+	void Start () {
+		HealthNumberGUI = GetComponent<Text> ();
+		DeathsNumberGUI = GetComponent<Text> ();
+	}
+
+
+	/*
+
+
+	 */
+
+}
