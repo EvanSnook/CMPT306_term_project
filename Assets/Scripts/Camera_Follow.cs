31e64c84cdf6d83de98aa7cd9620249321a86a3c
Moniloth\Jlehm <jlehmann96@gmail.com>
2016-11-02 19:39:58 -0600

    Changed Camera_Follow to follow smoothly, smoothness now set by a public variable "dampeningFactor", residing in said script, which is attached to the Main Camera. Also added a check to make sure player is not null to help make the code less prone to error.

--- Assets/Scripts/Camera_Follow.cs
+++ Assets/Scripts/Camera_Follow.cs
@@ -1,17 +1,27 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Camera_Follow : MonoBehaviour {
 
 	public GameObject player;
+	public float dampeningFactor;
+
+	private Vector3 velocity;
 
 	// Use this for initialization
 	void Start () {
-	
+		velocity = Vector3.zero;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		transform.position = new Vector3(player.transform.position.x,player.transform.position.y + 2.5f ,transform.position.z);
+		// Catch errors that may arrise from having the player die or disappear.
+		if (player != null) {
+
+			// Remove the Z axis from player position
+			Vector3 destination = new  Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+			// SmoothDamp automatically smooths out motion between two points, given the pounts, velocity, and a dampeningFactor
+			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampeningFactor);
+		}
 	}
 }
