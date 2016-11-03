dc9c5a2912d57c70ca48a44868edf389942604ab
Tim <XxWolf9630xX@gmail.com>
2016-11-02 23:33:03 -0600

    Created A Script that Saves Data from one Scene to the Next. Inside this object we can save Boss Health and Player Skill Points from when it goes between boss room and Spawn Room.

---
+++ Assets/Scripts/SavedData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SavedData : MonoBehaviour {
+
+	public float BossHealth;
+	public int SkillPoints;
+
+	void Start () {
+		DontDestroyOnLoad (gameObject);
+	}
+	
+}
