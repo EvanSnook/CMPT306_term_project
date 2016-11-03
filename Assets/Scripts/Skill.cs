114325ed5ea28b7ee0804b19892f867e342562e9
ADAM-PC\user <adam.v.r@hotmail.com>
2016-11-03 02:15:08 -0600

    Adding genric Skill and SkillTree scriptable classes.

---
+++ Assets/Scripts/Skill.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Skill : ScriptableObject {
+
+    string Name { get; set; }
+    string Desciptoin { get; set; }
+
+    Skill(string inName, string inDescription)
+    {
+        Name = inName;
+        Desciptoin = inDescription;
+    }
+}
