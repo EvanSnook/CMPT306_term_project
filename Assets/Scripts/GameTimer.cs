91feae93e616198fc93fc3156402813268a1de89
Tim <XxWolf9630xX@gmail.com>
2016-11-02 20:39:43 -0600

    Finished Basic Player GUI and Game Timer. Might Want to show less decimals or no decimals for the timer.

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameTimer : MonoBehaviour {
+
+	public float GameCountdown; // This is the time in the Game Left before you fail.
+
+	void Update () {
+		if (GameCountdown > 0) { // If the timer is not 0 then Count down.
+			GameCountdown -= Time.deltaTime;
+		} else if (GameCountdown < 0) { // If the Timer is below 0 then just set it to 0.
+			GameCountdown = 0;
+		}
+	}
+}
--- Assets/Scripts/PlayerGUIManager.cs
+++ Assets/Scripts/PlayerGUIManager.cs
@@ -4,21 +4,25 @@ using UnityEngine.UI;
 
 public class PlayerGUIManager : MonoBehaviour {
 
-	public GameObject MainGUI;
+	private GameObject HealthNumberGUI; // This is the Element that Holds how much Health the player has.
+	private GameObject TimerGUI;
 
-	Text HealthNumberGUI; // This is the Element that Holds how much Health the player has.
-	Text DeathsNumberGUI; // This is the Element that Holds how many times the player has died.
-
+	private Text HealthNumber; // This is the Text for the TextGUI
+	private Text Timer;
 
 	void Start () {
-		HealthNumberGUI = GetComponent<Text> ();
-		DeathsNumberGUI = GetComponent<Text> ();
+		HealthNumberGUI = GameObject.Find ("HealthNumber"); // Find the Object that holds that HealthNumberGUI.
+		TimerGUI = GameObject.Find("Timer");
+
+		HealthNumber = HealthNumberGUI.GetComponent<Text> (); // This is the text of the GUI object.
+		Timer = TimerGUI.GetComponent<Text>(); 
 	}
 
+	// Update all Parts of the GUI.
+	void Update() {
+		HealthNumber.text = "Health: " + gameObject.GetComponent<Health> ().HealthPoints;
+		Timer.text = "Time Remaining: " + gameObject.GetComponent<GameTimer> ().GameCountdown;
+	}
 
-	/*
-
-
-	 */
 
 }
