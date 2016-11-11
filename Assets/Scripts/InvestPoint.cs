9f9929d644e384d77579bffa294e1097134abce1
ADAM-PC\user <adam.v.r@hotmail.com>
2016-11-11 15:10:58 -0600

    Added clickable nodes in skills menu.

---
+++ Assets/Scripts/InvestPoint.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InvestPoint : MonoBehaviour {
+
+    SkillTree root;
+    public static int SkillPoints = 1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDown()
+    {
+            Debug.Log("leveled up "+ gameObject.name);
+            //SendMessage("LevelUp");
+    }
+}
