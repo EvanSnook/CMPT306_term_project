d463a91700706d9c2ee3635a77cc2efaaef3b308
Tim <XxWolf9630xX@gmail.com>
2016-11-16 13:25:32 -0600

    Created Several Scripts to keep track of how many times skills have been used and how much damage total they have each done. Added a isFullHealth function to the Health Script.

--- Assets/Scripts/BossAI/BossAIBasic.cs
+++ Assets/Scripts/BossAI/BossAIBasic.cs
@@ -7,23 +7,10 @@ public class BossAIBasic : MonoBehaviour {
 	public GameObject ThePlayer;
 	public GameObject SceneControllerObject;
 
-	public GameObject BossSkillSelectionAI;
-
-	public bool inBossRoom;
-
-
 	void Start () {
-		SceneControllerObject = GameObject.Find ("SceneManagerObject");
-
-		if (SceneControllerObject.GetComponent<SceneController> ().CurrentlyLoadedScene == "BossAITest") {
-			inBossRoom = true;
-		}
-		else {
-			inBossRoom = false;
-
+		if (gameObject.GetComponent<Health> ().isFullHealth ()) {
 
 		}
-
 	}
 
 
---
+++ Assets/Scripts/SavedData/PlayerRangedSkills.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerRangedSkills : MonoBehaviour {
+
+	public int NumberOfBasicRangedUsed; // This holds the number of times that the boss has used the Basic Ranged.
+	public int TotalDMGDoneWithRangedMelee; // This is the total about of Damage that has been done with the Basic Ranged.
+
+	public int NumberOfSkill1; // This is just a place holder for the future.
+	public int TotalDMGDoneWithSkill1; // This is just a place holder for the future.
+
+
+	void Start () {
+	
+	}
+
+
+}
