4a3cb3943abd4a22d8310de73af8c610b7eb58de
Justin <jlehmann96@gmail.com>
2016-11-15 21:09:06 -0600

    Added Pushable and PushonContact. Objects with PushOnContact will push objects with Pushable

---
+++ Assets/Scripts/Pushable.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pushable : MonoBehaviour {
+
+	private Rigidbody2D rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody2D>();
+	}
+
+	void Push(Vector2 direction) {
+		rigidBody.AddForce(direction);
+	}
+}
