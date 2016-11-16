1af5e88859805a0b819f8b528ab2a37f5c7d5458
Tim <XxWolf9630xX@gmail.com>
2016-11-15 18:27:00 -0600

    Added the player skill point aquisition over time

--- Assets/Scripts/GameTimer.cs
+++ Assets/Scripts/GameTimer.cs
@@ -1,15 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class GameTimer : MonoBehaviour {
 
 	public float GameCountdown; // This is the time in the Game Left before you fail.
+	public float TimePerSkillPoint; // This is how often the player will recieve a skill point for time.
+	public int SkillPointGain; // This is how many skill points are gained at each time point.
+	public GameObject SavedData; // This is a reference to the saved data that will hold any information that is needed to not be lost. 
+
+
+	void Start() {
+		InvokeRepeating("SendSkillPoint", TimePerSkillPoint, TimePerSkillPoint); // This will repeatedly send skillpoints to the saved data.
+		SavedData = GameObject.Find("SavedData"); // This finds and sets the Saved Data to the Saved Data Object.
+	}
 
 	void Update () {
 		if (GameCountdown > 0) { // If the timer is not 0 then Count down.
 			GameCountdown -= Time.deltaTime;
-		} else if (GameCountdown < 0) { // If the Timer is below 0 then just set it to 0.
-			GameCountdown = 0;
+		} else if (GameCountdown < 0) { // If the Timer is below 0 then load game over screen.
+			SceneManager.LoadScene("GameOver"); // Load the Game Over scene.
 		}
 	}
+
+	/*
+		This is the helper function that sends the number of skill points to the saved data.
+	 */
+	public void SendSkillPoint() {
+		SavedData.SendMessage ("AddSkillPoints", SkillPointGain);
+
+	}
+
 }
--- Assets/Scripts/SavedData.cs
+++ Assets/Scripts/SavedData.cs
@@ -3,11 +3,34 @@ using System.Collections;
 
 public class SavedData : MonoBehaviour {
 
-	public float BossHealth;
-	public int SkillPoints;
+	public float BossHealth; // This is the Health of the Boss that will be loaded.
+	public int SkillPoints; // This is the number of skill points that the player currently has.
+	public float TimeRemaining; // This is the time that is remaining for the player.
 
+
+	/*
+		This makes this object not be destroyed when switching scenes.
+	*/
 	void Start () {
 		DontDestroyOnLoad (gameObject);
+		SkillPoints = 0;
 	}
-	
+		
+	/*
+		This is called to add a specific number of points when sent message.
+	 */
+	public void AddSkillPoints(int NumberOfPoints) {
+		SkillPoints += NumberOfPoints;
+	}
+
+
+	/*
+		This will be invoked to save the health that the boss has when the player dies and goes back to the spawn room. 
+		The boss health will take this value when loading scene.
+	 */
+	public void SaveBossHealth(float Health) {
+		BossHealth = Health;
+	}
+
+
 }
