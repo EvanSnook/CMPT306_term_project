6eb7e317ebbb54b6bdd8a58e7520bec83cb7f649
Evan <snovan@hotmail.com>
2016-11-16 12:47:09 -0600

    pulse works - but DMG is broke asf

--- Assets/Scripts/Q/ScreenClearingPulse.cs
+++ Assets/Scripts/Q/ScreenClearingPulse.cs
@@ -3,13 +3,43 @@ using System.Collections;
 
 public class ScreenClearingPulse : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    public float cooldownDuration;
+    public float despawnTime;
+    public GameObject shieldPrefab;
+
+    private GameObject shield;
+    private bool canShield;
+
+    // Use this for initialization
+    void Start () {
+        canShield = true;
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+	    if(shield != null)
+        {
+            shield.transform.localScale += new Vector3(1f,1f,0); 
+        }
 	}
+
+    void UseShield()
+    {
+        if (canShield)
+        {
+            canShield = false;
+            shield = Instantiate(shieldPrefab, transform.position, transform.rotation) as GameObject;
+            shield.GetComponent<SpriteRenderer>().material.color = new Color(1f, 1f, 1f, 0.5f);
+            Destroy(shield,despawnTime);
+            StartCoroutine("shieldCooldown");
+        }
+    }
+
+
+    IEnumerator shieldCooldown()
+    {
+        yield return new WaitForSeconds(cooldownDuration);
+        canShield = true; ;
+    }
 }
