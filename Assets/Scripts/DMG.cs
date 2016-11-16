ab1bcc3fc49082f18aecbbe617bd3b619ffaa49b
Justin <jlehmann96@gmail.com>
2016-11-15 18:32:25 -0600

    Added new public variable to DMG, damageRepeat contains a float that says how many seconds before the same object takes damage multiple times from a hitbox. If set to zero, the hitbox will just hit once

--- Assets/Scripts/DMG.cs
+++ Assets/Scripts/DMG.cs
@@ -1,10 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class DMG : MonoBehaviour {
 	//public string DMGTargetTag;
 	public int DMGDone;
+	public float DamageRepeatTime;
 	public GameObject Owner;
+
+	private List<GameObject> CollidingWith = new List<GameObject>();
 
 	void SetOwner (GameObject OwnerIn) {
 		Owner = OwnerIn;
@@ -13,12 +17,44 @@ public class DMG : MonoBehaviour {
 	void OnCollisionEnter2D(Collision2D coll) {
 		if (coll.gameObject.GetComponent("Health") != null && coll.gameObject != Owner) {
 			coll.gameObject.SendMessage("ApplyDMG", DMGDone);
+			if (DamageRepeatTime > 0) {
+				CollidingWith.Add(coll.gameObject);
+				StartCoroutine("DamageRepeat", coll.gameObject);
+			}
 		}
 	}
 
 	void OnTriggerEnter2D(Collider2D coll) {
 		if (coll.gameObject.GetComponent("Health") != null && coll.gameObject != Owner) {
 			coll.gameObject.SendMessage("ApplyDMG", DMGDone);
+			if (DamageRepeatTime > 0) {
+				CollidingWith.Add(coll.gameObject);
+				StartCoroutine("DamageRepeat", coll.gameObject);
+			}
+		}
+	}
+
+	void OnCollisionExit2D(Collision2D coll) {
+		if (CollidingWith.Contains(coll.gameObject)) {
+			CollidingWith.Remove(coll.gameObject);
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D coll) {
+		if (CollidingWith.Contains(coll.gameObject)) {
+			CollidingWith.Remove(coll.gameObject);
+		}
+	}
+
+	IEnumerator DamageRepeat(GameObject theObject) {
+		yield return new WaitForSeconds(DamageRepeatTime);
+		if (theObject != null) {
+			if (CollidingWith.Contains(theObject)) {
+
+				theObject.SendMessage("ApplyDMG", DMGDone);
+
+				StartCoroutine("DamageRepeat", theObject);
+			}
 		}
 	}
 }
