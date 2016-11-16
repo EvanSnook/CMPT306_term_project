c132c71b0cd6fb146778109178234db86c32d4b8
Evan <snovan@hotmail.com>
2016-11-15 20:40:38 -0600

    shield follows mouse on fixed update now and omni shield ability works

--- Assets/Scripts/Q/UndirectedShield.cs
+++ Assets/Scripts/Q/UndirectedShield.cs
@@ -18,7 +18,7 @@ public class UndirectedShield : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (isShielding)
         {
