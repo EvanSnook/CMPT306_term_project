eb7c7cce3ad5083b9727c5f9956bc1368662ccd1
Justin <jlehmann96@gmail.com>
2016-11-15 19:46:02 -0600

    - Fixed a bug with Melee setOwner - Added FlashOnDamage, making the boss flash when hurt - Added DMG to boss prefab, allowing the boss to deal damage

---
+++ Assets/Scripts/FlashOnDamage.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlashOnDamage : MonoBehaviour {
+	public float FlashDuration;
+
+	private SpriteRenderer Sprite;
+
+	void Start() {
+		Sprite = GetComponent<SpriteRenderer>();
+	}
+
+	public void ApplyDMG(int DMG) {
+		Sprite.color = Color.white;
+		StartCoroutine("RevertColor");
+	}
+
+	IEnumerator RevertColor() {
+		yield return new WaitForSeconds(FlashDuration);
+		Sprite.color = Color.red;
+	}
+}
