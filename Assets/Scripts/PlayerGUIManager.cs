8ebc52c90941bc57ea36b81f5bbcfa73269a755c
Tim <XxWolf9630xX@gmail.com>
2016-11-16 11:27:09 -0600

    Fixed Game Timer to be its own object, fixed Player basic UI to be its own object, added SavedStats to the Saved Data object.

--- Assets/Scripts/PlayerGUIManager.cs
+++ Assets/Scripts/PlayerGUIManager.cs
@@ -4,24 +4,29 @@ using UnityEngine.UI;
 
 public class PlayerGUIManager : MonoBehaviour {
 
-	private GameObject HealthNumberGUI; // This is the Element that Holds how much Health the player has.
-	private GameObject TimerGUI;
+	public GameObject Player; // This is the Player where you can get the Health.
+	public GameObject TimerObject; // This is the TimerObject where you can get the Time remaining.
 
-	private Text HealthNumber; // This is the Text for the TextGUI
-	private Text Timer;
+	public GameObject HealthNumberGUI; // This is the Element that Holds how much Health the player has.
+	public GameObject TimerGUI;
+
+	private Text HealthNumber; // This is the Text for the TextGUI that holds the HealthNumber.
+	private Text Timer; // This is the Text for the TextGUI that holds the Timer.
 
 	void Start () {
+		TimerObject = GameObject.Find("TimerObject"); // This finds the object that Holds the Timer.
+
 		HealthNumberGUI = GameObject.Find ("HealthNumber"); // Find the Object that holds that HealthNumberGUI.
 		TimerGUI = GameObject.Find("Timer");
 
 		HealthNumber = HealthNumberGUI.GetComponent<Text> (); // This is the text of the GUI object.
-		Timer = TimerGUI.GetComponent<Text>(); 
+		Timer = TimerGUI.GetComponent<Text>(); // This is the text of the GUI object for the Timer.
 	}
 
 	// Update all Parts of the GUI.
 	void Update() {
-		HealthNumber.text = "Health: " + gameObject.GetComponent<Health> ().HealthPoints;
-		Timer.text = "Time Remaining: " + gameObject.GetComponent<GameTimer> ().GameCountdown;
+		HealthNumber.text = "Health: " + Player.GetComponent<Health> ().HealthPoints; // This gets the current Health Component and sets the GUI object to it.
+		Timer.text = "Time Remaining: " + TimerObject.GetComponent<GameTimer> ().GameCountdown; // This gets the current TimeRemaining and sets the GUI object to it.
 	}
 
 
