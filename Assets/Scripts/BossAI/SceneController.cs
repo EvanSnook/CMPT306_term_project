1b807ff724365024eb1f601bfd53ba1b2c69413d
Tim <XxWolf9630xX@gmail.com>
2016-11-15 18:57:58 -0600

    Started Working on the Boss AI. Created a few Scripts to Manage the Different information

---
+++ Assets/Scripts/BossAI/SceneController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+
+	public string CurrentlyLoadedScene;
+
+	void Start () {
+		CurrentlyLoadedScene = SceneManager.GetActiveScene().name;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
