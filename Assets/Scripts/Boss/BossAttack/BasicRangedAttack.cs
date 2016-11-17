aa6cd3541c2f31d770fb93e7633ec861431d0820
Tim <XxWolf9630xX@gmail.com>
2016-11-17 14:09:40 -0600

    Reorganized a bunch of the Scripts into folders. Have a child object of the boss that is supposed to rotate towards the player but isn't rotating properly.

---
+++ Assets/Scripts/Boss/BossAttack/BasicRangedAttack.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicRangedAttack : MonoBehaviour {
+
+	private GameObject ProjectileLauncher; // This is the Projectile Launcher Object.
+	public GameObject Player; // This is the player that is being targeted.
+	public float DistanceFromCenter; // This is how far away the Projectile Launcher is from the center of the boss.
+
+	void Start () {
+		ProjectileLauncher = gameObject.transform.GetChild (0).gameObject; // This gets the child object which in this case is the projectile launcher.
+	}
+
+	void Update () {
+		Vector3 PlayerPosition = Player.transform.position; // Get the Mouse Position.
+
+		PlayerPosition.z = transform.position.z - Camera.main.transform.position.z;
+		PlayerPosition = Camera.main.ScreenToWorldPoint(PlayerPosition);
+
+		Quaternion AngleToPlayer = Quaternion.FromToRotation(Vector3.right, PlayerPosition - transform.position);
+
+		ProjectileLauncher.transform.rotation = AngleToPlayer;
+		// Current Problem is it is not rotating properly.
+	}
+
+}
