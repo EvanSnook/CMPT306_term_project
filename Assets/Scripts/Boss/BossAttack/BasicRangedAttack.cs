980b8433773cd943a00b88b31e437fb68774bf20
Tim <XxWolf9630xX@gmail.com>
2016-11-19 00:16:05 -0600

    Got projectiles firing towards the player. Next step is setting up a firing solution and Fixing having the launcher move around the edge of the boss.

--- Assets/Scripts/Boss/BossAttack/BasicRangedAttack.cs
+++ Assets/Scripts/Boss/BossAttack/BasicRangedAttack.cs
@@ -6,7 +6,7 @@ public class BasicRangedAttack : MonoBehaviour {
 	private GameObject ProjectileLauncher; // This is the Projectile Launcher Object.
 	public GameObject Player; // This is the player that is being targeted.
 	public float DistanceFromCenter; // This is how far away the Projectile Launcher is from the center of the boss.
-	private GameObject Clone;
+//	private GameObject Clone;
 
 	void Start () {
 		ProjectileLauncher = gameObject.transform.GetChild (0).gameObject; // This gets the child object which in this case is the projectile launcher.
@@ -15,14 +15,14 @@ public class BasicRangedAttack : MonoBehaviour {
 	void Update () {
 		Vector3 PlayerPosition = Player.transform.position;
 
-		Quaternion AngleToPlayer = Quaternion.FromToRotation(Vector3.right, PlayerPosition - transform.position);
-		Clone = Instantiate(ProjectileLauncher, transform.position, AngleToPlayer) as GameObject;
+		Quaternion AngleToPlayer = Quaternion.FromToRotation(Vector3.up, PlayerPosition - transform.position );
+//		Clone = Instantiate(ProjectileLauncher, transform.position, AngleToPlayer) as GameObject;
 
+		ProjectileLauncher.transform.rotation = AngleToPlayer;
+		ProjectileLauncher.transform.parent = gameObject.transform;
+//		ProjectileLauncher.transform.Translate(new Vector3 (DistanceFromCenter, 0, 0));
 
-		Clone.transform.parent = gameObject.transform;
-		Clone.transform.Translate(new Vector3 (DistanceFromCenter, 0, 0));
-
-		Destroy(Clone, 2);
+//		Destroy(Clone, 2);
 	}
 
 }
