737565781e9a277bf609b628c30c2d4edc2318e4
Tim <XxWolf9630xX@gmail.com>
2016-11-22 17:35:10 -0600

    Fully Fixed Scene Changes

--- Assets/Scripts/GameComponenets/SceneController.cs
+++ Assets/Scripts/GameComponenets/SceneController.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 public class SceneController : MonoBehaviour {
 
 	public string CurrentlyLoadedScene;
+	private string LevelName;
 
 	// This gets the scene that is currently loaded.
 	void Start () {
@@ -13,24 +14,40 @@ public class SceneController : MonoBehaviour {
 
 	// This is called when the game is over and will go to the game over screen.
 	public void ChangeSceneGameOver() {
-		ChangeLevel ("game_over");
+		LevelName = "game_over";
+		StartCoroutine ("ChangeLevel");
 	}
 
 	// This looks at what the current scene is and then changes scene respectively.
 	public void ChangeScene() {
 		if (CurrentlyLoadedScene == "boss_room") { // If the current scene is boss_room then change scene to the spawn_room else, change to the boss_room. 
-			ChangeLevel("spawn_room");
+			LevelName = "spawn_room";
+			StartCoroutine ("ChangeLevel");
 		} else {
-			ChangeLevel("boss_room");
+			LevelName = "boss_room";
+			StartCoroutine ("ChangeLevel");
+		}
+	}
+
+	public void ChangeTestScene() {
+		if (CurrentlyLoadedScene == "TestScene1") { // This is for testing the change scene. 
+			LevelName = "TestScene2";
+			StartCoroutine ("ChangeLevel");
+		} else {
+			LevelName = "TestScene1";
+			StartCoroutine ("ChangeLevel");
 		}
 	}
 
 
 	// This will change to the LevelName.
-	private IEnumerator ChangeLevel(string LevelName) {
-		float FadeTime = gameObject.GetComponent<SceneTransition>().BeginFade (1); // This begins the fade between scenes.
+	IEnumerator ChangeLevel() {
+		Debug.Log ("Begin Fadeing");
+		float FadeTime = gameObject.GetComponent<SceneTransition>().BeginFade(1); // This begins the fade between scenes.
 		yield return new WaitForSeconds (FadeTime);
 		SceneManager.LoadScene (LevelName);
 	}
+
+
 
 }
