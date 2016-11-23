7669574d2ed21945f9d4cbccb0a101beea7b4c7e
Tim <XxWolf9630xX@gmail.com>
2016-11-23 15:01:13 -0600

    Created a Object that holds a script that currently just pauses the game if you hit escape. It will eventually also open or turn on a menu screen.

---
+++ Assets/Scripts/GameComponenets/PopUpMenu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PopUpMenu : MonoBehaviour {
+
+	public bool GamePaused;
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)) { // If the Player Hits the Excape key pause the game and open a menu.
+			TogglePauseGame();
+		}
+		if (GamePaused) {
+
+		}
+	}
+
+	// This pauses and resumes the game by alternating the time scale between 1 and 0.
+	public void TogglePauseGame() {
+		if (Time.timeScale == 0f) {
+			Time.timeScale = 1f;
+			GamePaused = false;
+		} else {
+			Time.timeScale = 0f;
+			GamePaused = true;
+		}
+	}
+
+}
