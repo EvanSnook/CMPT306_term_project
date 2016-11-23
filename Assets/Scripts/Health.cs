04be5228cd955c1ee4a6d11f95b6aa098a58f3d8
Justin <jlehmann96@gmail.com>
2016-11-22 22:20:23 -0600

    --- CURRENTLY BROKEN --- - Fixed Health being set to zero on both character and boss - Changed the way death works (CURRENTLY BROKEN)

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -3,18 +3,12 @@ using System.Collections;
 
 public class Health : MonoBehaviour {
 
-	public int HealthPoints; // This is the total number of Health this Object Has.
+	public int HealthPoints; // This is the total number of Health this Object Has
 
-	private bool isPlayer; // If this is the player have it's death do something different.
 	private float StartingHealthPoints; // This is the health points that the player has at the start.
 
 	void Start() {
 		StartingHealthPoints = HealthPoints; // This just sets the StartingHealthPoints to the health points that were there when they started.
-		if (gameObject.tag == "Player") { // Check if the current object is the player or not.
-			isPlayer = true;
-		} else {
-			isPlayer = false;
-		}
 	}
 
 
@@ -29,11 +23,10 @@ public class Health : MonoBehaviour {
 	public void ApplyDMG(int DMG) {
 		HealthPoints -= DMG;
 	}
-    // This is what happends when dead.
-    public void Death() {
-        if (!isPlayer) { // If this is the player then do not destroy it
-            Destroy(gameObject);
-        }
+	
+  // This is what happends when dead.
+  public void Death() {
+			Destroy(gameObject);
 	}
 
 	// This checks to see if this is at full health.
---
+++ Assets/Scripts/Player/PlayerOnDeath.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerOnDeath : MonoBehaviour {
+
+	public GameObject SceneController; // Contains a reference to the Scene Controller Object
+
+	void OnDestroy() {
+		SceneController.SendMessage("ChangeLevel"); // When the player is killed, change the sene
+	}
+
+}
