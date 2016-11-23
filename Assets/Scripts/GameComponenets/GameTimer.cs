e8e1808660f29397a33aba57397d85db6049d320
Tim <XxWolf9630xX@gmail.com>
2016-11-23 11:02:12 -0600

    - Fixed Scene Transition between Spawn Room and Boss Room by adding scene transition object to spawn room. - Added more coments to Game TImer, Scene Controller. Removed Saved Data Object from Boss Room.      You need to Start in the Spawn Room and the saved data follows you into the boss room. Game Timer "Time" is set in the saved data so that when player dies it can save the time remaining and reset it when moveing back to boss room.

--- Assets/Scripts/GameComponenets/GameTimer.cs
+++ Assets/Scripts/GameComponenets/GameTimer.cs
@@ -1,27 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.SceneManagement;
 
 public class GameTimer : MonoBehaviour {
 
-	public float GameCountdown; // This is the time in the Game Left before you fail.
+	[HideInInspector] public float GameCountdown; // This is the time in the Game Left before you fail. It is set from the Saved data object.
 	public float TimePerSkillPoint; // This is how often the player will recieve a skill point for time.
 	public int SkillPointGain; // This is how many skill points are gained at each time point.
 	public GameObject SavedData; // This is a reference to the saved data that will hold any information that is needed to not be lost. 
 
+	public GameObject SceneControllerObject; // This is a reference to the onject that changes scenes.
 
 	void Start() {
 		SavedData = GameObject.Find("SavedData"); // This finds and sets the Saved Data to the Saved Data Object.
 		InvokeRepeating("SendSkillPoint", TimePerSkillPoint, TimePerSkillPoint); // This will repeatedly send skillpoints to the saved data.
 
 		GameCountdown = SavedData.GetComponent<SavedData>().TimeRemaining; // This gets and sets the Time Remaining from the saved data.
+
+		SceneControllerObject = GameObject.Find ("SceneControllerObject"); // This gets the reference to the sceneControllerObject.
 	}
 
+	// This runs to countdown and if it hits zero then it is game over.
 	void Update () {
 		if (GameCountdown > 0) { // If the timer is not 0 then Count down.
 			GameCountdown -= Time.deltaTime;
 		} else if (GameCountdown < 0) { // If the Timer is below 0 then load game over screen.
-			SceneManager.LoadScene("GameOver"); // Load the Game Over scene.
+			SceneControllerObject.GetComponent<SceneController>().SendMessage("ChangeSceneGameOver"); // This sends a message to that the game is over.
 		}
 	}
 
--- Assets/Scripts/GameComponenets/SceneController.cs
+++ Assets/Scripts/GameComponenets/SceneController.cs
@@ -4,31 +4,32 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour {
 
-	public string CurrentlyLoadedScene;
-	private string LevelName;
+	public string CurrentlyLoadedScene; // This holds the name of the currently loaded scene.
+	private string LevelName; // This will hold the name of the scene that will be loaded if there needs to be a scene change.
 
 	// This gets the scene that is currently loaded.
 	void Start () {
-		CurrentlyLoadedScene = SceneManager.GetActiveScene().name;
+		CurrentlyLoadedScene = SceneManager.GetActiveScene().name; // This gets the name of the scene that is currently loaded.
 	}
 
 	// This is called when the game is over and will go to the game over screen.
 	public void ChangeSceneGameOver() {
-		LevelName = "game_over";
-		StartCoroutine ("ChangeLevel");
+		LevelName = "game_over"; // This is the level name that the scene will be changed to.
+		StartCoroutine ("ChangeLevel"); // This is the call to change scene.
 	}
 
 	// This looks at what the current scene is and then changes scene respectively.
 	public void ChangeScene() {
 		if (CurrentlyLoadedScene == "boss_room") { // If the current scene is boss_room then change scene to the spawn_room else, change to the boss_room. 
-			LevelName = "spawn_room";
-			StartCoroutine ("ChangeLevel");
+			LevelName = "spawn_room"; // This is the level name that the scene will be changed to.
+			StartCoroutine ("ChangeLevel"); // This is the call to change scene.
 		} else {
-			LevelName = "boss_room";
-			StartCoroutine ("ChangeLevel");
+			LevelName = "boss_room"; // This is the level name that the scene will be changed to.
+			StartCoroutine ("ChangeLevel"); // This is the call to change scene.
 		}
 	}
 
+	// This is only used in the test scenes and will be removed in final product.
 	public void ChangeTestScene() {
 		if (CurrentlyLoadedScene == "TestScene1") { // This is for testing the change scene. 
 			LevelName = "TestScene2";
@@ -42,10 +43,9 @@ public class SceneController : MonoBehaviour {
 
 	// This will change to the LevelName.
 	IEnumerator ChangeLevel() {
-		Debug.Log ("Begin Fadeing");
 		float FadeTime = gameObject.GetComponent<SceneTransition>().BeginFade(1); // This begins the fade between scenes.
-		yield return new WaitForSeconds (FadeTime);
-		SceneManager.LoadScene (LevelName);
+		yield return new WaitForSeconds (FadeTime); // This makes it wait until it has fully faded out.
+		SceneManager.LoadScene (LevelName); // This changes the scene.
 	}
 
 
