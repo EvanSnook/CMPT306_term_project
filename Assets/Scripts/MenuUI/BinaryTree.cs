6063307b0a7cd8b6f3dcc20d2f1f264c857a4573
ADAM-PC\user <adam.v.r@hotmail.com>
2016-11-25 01:13:50 -0600

    Added a binary tree that holds the ui nodes.

---
+++ Assets/Scripts/MenuUI/BinaryTree.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BinaryTree : ScriptableObject {
+
+    public BinaryTree Parent;
+    public BinaryTree RightChild;
+    public BinaryTree LeftChild;
+    
+    public GameObject UINode;
+    
+    public BinaryTree(BinaryTree inParent, GameObject inElement)
+    {
+        Parent = inParent;
+        UINode = inElement;
+    }
+
+    public BinaryTree getParent()
+    {
+        return Parent;
+    }
+
+    public BinaryTree getRightChild()
+    {
+        return RightChild;
+    }
+
+    public BinaryTree getLeftChild()
+    {
+        return LeftChild;
+    }
+
+    public GameObject getSkillTreeNode()
+    {
+        return UINode;
+    }
+
+    
+}
---
+++ Assets/Scripts/SavedData/SkillSavedData.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkillSavedData : MonoBehaviour {
+
+    BinaryTree QTree;
+    public GameObject QTreeRootNode;
+	// Use this for initialization
+	void Start () {
+        QTree = generateTree(QTreeRootNode);
+    }
+	
+    public BinaryTree generateTree(GameObject Item)
+    {
+        BinaryTree Root;
+        Root = (BinaryTree)ScriptableObject.CreateInstance("BinaryTree");
+        Root.UINode = Item;
+        Debug.Log(Time.time + " "+Root.UINode.name);
+        foreach (Transform child in Item.transform)
+        {
+            if (child.gameObject.tag == "SkillTreeNode")
+            {
+                if (Root.getRightChild() == null)
+                {
+                    Root.RightChild = generateTree(child.gameObject);
+                }
+                else if (Root.getLeftChild() == null)
+                {
+                    Root.LeftChild = generateTree(child.gameObject);
+                }
+            }
+        }
+        
+        return Root;
+    }
+}
