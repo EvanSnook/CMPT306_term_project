26644457de6178b9e328f9436831696f62791710
Moniloth\Jlehm <jlehmann96@gmail.com>
2016-11-26 04:21:29 -0600

    Pulled out some magic magic numbers for the swing sizes and made them public variables. Cleaned up the public variables in Melee, adding headers and sliders

--- Assets/Scripts/Player/Melee.cs
+++ Assets/Scripts/Player/Melee.cs
@@ -4,12 +4,23 @@ using System.Collections;
 public class Melee : MonoBehaviour {
 
 	public float cooldownDuration;
+
+	public float comboTimeout;
+	public float pushIntensity;
+
+	[Header("Swing Prefabs")]
 	public GameObject swing1Prefab;
 	public GameObject swing2Prefab;
 	public GameObject swing3Prefab;
-	public float swingSize;
-	public float comboTimeout;
-	public float pushIntensity;
+	
+	[Header("Prefab Sizes")]
+	[Range(0.1f, 1f)]
+	public float swing1Size;
+	[Range(0.1f, 1f)]
+	public float swing2Size;
+	[Range(0.1f, 1f)]
+	public float swing3Size;
+	[Space(20)]
 
 	private GameObject swing; // The swing itself
 	private bool canSwing;
@@ -42,17 +53,17 @@ public class Melee : MonoBehaviour {
 			switch (comboState) {
 				case 0:
 					swing = Instantiate(swing1Prefab, transform.position, AngleToMouse) as GameObject;
-					swing.transform.localScale = new Vector3(swingSize, swingSize, swingSize);
+					swing.transform.localScale = new Vector3(swing1Size, swing1Size, swing1Size);
 					comboState = 1;
 					break;
 				case 1:
 					swing = Instantiate(swing2Prefab, transform.position, AngleToMouse) as GameObject;
-					swing.transform.localScale = new Vector3(swingSize*1.2f, swingSize*1.2f, swingSize*1.2f);
+					swing.transform.localScale = new Vector3(swing2Size, swing2Size, swing2Size);
 					comboState = 2;
 					break;
 				case 2:
 					swing = Instantiate(swing3Prefab, transform.position, AngleToMouse) as GameObject;
-					swing.transform.localScale = new Vector3(swingSize*2f, swingSize*2f, swingSize*2f);
+					swing.transform.localScale = new Vector3(swing3Size, swing3Size, swing3Size);
 					gameObject.SendMessage("Push", new Vector2(MousePosition.x - transform.position.x, MousePosition.y - transform.position.y).normalized * pushIntensity);
 					comboState = 0;
 					break;
