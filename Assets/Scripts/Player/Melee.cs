3b73adb32a15d2771dd3f6677737d1e0dc3edac9
Moniloth\Jlehm <jlehmann96@gmail.com>
2016-11-26 03:39:55 -0600

    Holy shit so many files I'm sorry.
    Added third swing of basic melee combo

--- Assets/Scripts/Player/Melee.cs
+++ Assets/Scripts/Player/Melee.cs
@@ -6,7 +6,9 @@ public class Melee : MonoBehaviour {
 	public float cooldownDuration;
 	public GameObject swing1Prefab;
 	public GameObject swing2Prefab;
+	public GameObject swing3Prefab;
 	public float swingSize;
+	public float comboTimeout;
 
 	private GameObject swing; // The swing itself
 	private bool canSwing;
@@ -27,6 +29,9 @@ public class Melee : MonoBehaviour {
 	void SwingAtMouse () {
 		if (canSwing) {
 			canSwing = false;
+			if (Time.time - timeOfLastSwing > comboTimeout) {
+				comboState = 0;
+			}
 
 			Vector3 MousePosition = Input.mousePosition; // Get the Mouse Position.
 			MousePosition.z = transform.position.z - Camera.main.transform.position.z;
@@ -36,18 +41,25 @@ public class Melee : MonoBehaviour {
 			switch (comboState) {
 				case 0:
 					swing = Instantiate(swing1Prefab, transform.position, AngleToMouse) as GameObject;
+					swing.transform.localScale = new Vector3(swingSize, swingSize, swingSize);
 					comboState = 1;
 					break;
 				case 1:
 					swing = Instantiate(swing2Prefab, transform.position, AngleToMouse) as GameObject;
+					swing.transform.localScale = new Vector3(swingSize*1.2f, swingSize*1.2f, swingSize*1.2f);
+					comboState = 2;
+					break;
+				case 2:
+					swing = Instantiate(swing3Prefab, transform.position, AngleToMouse) as GameObject;
+					swing.transform.localScale = new Vector3(swingSize*2f, swingSize*2f, swingSize*2f);
 					comboState = 0;
 					break;
 				default:
 					break;
 			}
 			swing.SendMessage("SetOwner", gameObject);
-			swing.transform.localScale = new Vector3(swingSize, swingSize, swingSize);
 			swing.transform.parent = gameObject.transform;
+			timeOfLastSwing = Time.time;
 
 			StartCoroutine("RefreshSwing");
 		}
