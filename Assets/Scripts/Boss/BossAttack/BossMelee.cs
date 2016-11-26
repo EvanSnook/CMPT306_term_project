e7b9af32a0fd94bc73fdf137ab55d785a6a3dc0f
Evan <snovan@hotmail.com>
2016-11-26 00:01:05 -0600

    initial test setup boss creates melee object when player is close added folders

---
+++ Assets/Scripts/Boss/BossAttack/BossMelee.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossMelee : MonoBehaviour {
+
+    public GameObject meleePrefab;
+    public float meleeDistance;
+    public float coolDownTimer;
+    public float meleeDuration;
+    public float meleeSpeed;
+
+    private GameObject player;
+    private GameObject meleeAttack;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        if (Vector3.Distance(player.transform.position, gameObject.transform.position) <= meleeDistance)
+        {
+            meleeAttack = Instantiate(meleePrefab, transform.position, transform.rotation) as GameObject;
+        }
+    }
+}
