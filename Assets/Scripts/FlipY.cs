5aca189d961f290061781152cc7d8fda8a596750
Moniloth\Jlehm <jlehmann96@gmail.com>
2016-11-26 00:23:25 -0600

    Added a second swing direction for basic melee attacks, moved melee prefabs into seperate folder

---
+++ Assets/Scripts/FlipY.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlipY : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.localScale = new Vector3(transform.localScale.x, -transform.localScale.y, transform.localScale.z);
+	}
+}
--- Assets/Scripts/Player/Melee.cs
+++ Assets/Scripts/Player/Melee.cs
@@ -4,15 +4,19 @@ using System.Collections;
 public class Melee : MonoBehaviour {
 
 	public float cooldownDuration;
-	public GameObject swingPrefab;
+	public GameObject swing1Prefab;
+	public GameObject swing2Prefab;
 	public float swingSize;
 
 	private GameObject swing; // The swing itself
 	private bool canSwing;
+	private int comboState;
+	private float timeOfLastSwing;
 
 	// Use this for initialization
 	void Start () {
 		canSwing = true;
+		comboState = 0;
 	}
 
 	// Update is called once per frame
@@ -27,9 +31,20 @@ public class Melee : MonoBehaviour {
 			Vector3 MousePosition = Input.mousePosition; // Get the Mouse Position.
 			MousePosition.z = transform.position.z - Camera.main.transform.position.z;
 			MousePosition = Camera.main.ScreenToWorldPoint(MousePosition);
-
 			Quaternion AngleToMouse = Quaternion.FromToRotation(Vector3.right, MousePosition - transform.position);
-			swing = Instantiate(swingPrefab, transform.position, AngleToMouse) as GameObject;
+
+			switch (comboState) {
+				case 0:
+					swing = Instantiate(swing1Prefab, transform.position, AngleToMouse) as GameObject;
+					comboState = 1;
+					break;
+				case 1:
+					swing = Instantiate(swing2Prefab, transform.position, AngleToMouse) as GameObject;
+					comboState = 0;
+					break;
+				default:
+					break;
+			}
 			swing.SendMessage("SetOwner", gameObject);
 			swing.transform.localScale = new Vector3(swingSize, swingSize, swingSize);
 			swing.transform.parent = gameObject.transform;
