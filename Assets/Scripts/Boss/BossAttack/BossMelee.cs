ba271f24fdb76b4aaa22928e1eaec6a922369cc2
Evan <snovan@hotmail.com>
2016-11-26 21:26:13 -0600

    boss swings, but not always the correct manner

--- Assets/Scripts/Boss/BossAttack/BossMelee.cs
+++ Assets/Scripts/Boss/BossAttack/BossMelee.cs
@@ -14,13 +14,27 @@ public class BossMelee : MonoBehaviour {
     private GameObject meleeAttack;
     private bool isAttacking;
     private bool onCooldown;
+    private Quaternion angleToPlayer;
+    private Vector3 startAngleFromPlayer;
+    private float edgeAt45DegOut;
+    private int attackDirection;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         isAttacking = false;
         onCooldown = false;
-	}
+        setRadiusAt45();
+
+        startAngleFromPlayer = new Vector3(startDistanceFromPlayer, startDistanceFromPlayer, 0f);
+    }
 	
+    //creates a float thats on the edge of the boss 45 degrees out
+    void setRadiusAt45()
+    {
+        float radius = transform.localScale.x / 2;
+        edgeAt45DegOut = radius * Mathf.Cos(45 * Mathf.PI / 180);
+    }
+
 	// Update is called once per frame
 	void Update () {
         player = GameObject.FindGameObjectWithTag("Player");
@@ -31,20 +45,62 @@ public class BossMelee : MonoBehaviour {
             {
                 isAttacking = true;
                 onCooldown = true;
-                meleeAttack = Instantiate(meleePrefab, transform.position, transform.rotation) as GameObject;
+
+                FindAngleOfAttack();
+
+                meleeAttack = Instantiate(meleePrefab, transform.position, angleToPlayer) as GameObject;
 
                 //parent the object to the boss
                 meleeAttack.transform.parent = gameObject.transform;
-
-                //positions the attack abit away from the player
-                meleeAttack.transform.rotation = new Quaternion (player.transform.position.x - transform.position.x, player.transform.position.y - transform.position.y,0f,0f);
-
+                
                 StartCoroutine("MeleeDuration");
                 StartCoroutine("CooldownTimer");
             }
         }
+        if(meleeAttack != null )
+        {
+            if (attackDirection != 0)
+            {
+                meleeAttack.transform.Rotate(0f, 0f, meleeSpeed * attackDirection);
+            }
+            else
+            {
+                //JAB
+            }
+        }
     }
 
+    private void FindAngleOfAttack()
+    {
+        //top x works
+        //bottom x distance too far
+        //right y distance too close
+        //left y works?
+        if      ((player.transform.position.x > gameObject.transform.position.x + edgeAt45DegOut && player.GetComponent<Rigidbody2D>().velocity.y > 0) ||
+                 (player.transform.position.x < gameObject.transform.position.x - edgeAt45DegOut && player.GetComponent<Rigidbody2D>().velocity.y < 0) ||
+                 (player.transform.position.y > gameObject.transform.position.x + edgeAt45DegOut && player.GetComponent<Rigidbody2D>().velocity.x < 0) ||
+                 (player.transform.position.y < gameObject.transform.position.x - edgeAt45DegOut && player.GetComponent<Rigidbody2D>().velocity.x > 0))
+        {
+            startAngleFromPlayer = new Vector3(startDistanceFromPlayer, startDistanceFromPlayer, 0f);
+            angleToPlayer = Quaternion.FromToRotation(Vector3.zero, player.transform.position - transform.position + startAngleFromPlayer);
+            attackDirection = -1;
+        }
+        else if ((player.transform.position.x > gameObject.transform.position.x + edgeAt45DegOut && player.GetComponent<Rigidbody2D>().velocity.y < 0) ||
+                 (player.transform.position.x < gameObject.transform.position.x - edgeAt45DegOut && player.GetComponent<Rigidbody2D>().velocity.y > 0) ||
+                 (player.transform.position.y > gameObject.transform.position.x + edgeAt45DegOut && player.GetComponent<Rigidbody2D>().velocity.x > 0) ||
+                 (player.transform.position.y < gameObject.transform.position.x - edgeAt45DegOut && player.GetComponent<Rigidbody2D>().velocity.x < 0))
+        {
+            startAngleFromPlayer = new Vector3(-startDistanceFromPlayer, -startDistanceFromPlayer, 0f);
+            angleToPlayer = Quaternion.FromToRotation(-Vector3.right, player.transform.position - transform.position - startAngleFromPlayer);
+            attackDirection = 1;
+        }
+
+        else {
+            angleToPlayer = Quaternion.FromToRotation(Vector3.right, player.transform.position - transform.position);
+            attackDirection = 0;
+        }
+
+    }
     IEnumerator MeleeDuration()
     {
         yield return new WaitForSeconds(meleeDuration);
