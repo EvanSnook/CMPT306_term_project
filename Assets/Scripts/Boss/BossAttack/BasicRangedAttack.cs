6bfe15ce01b4aae5afd98919b2acd5f8838e1962
Evan <snovan@hotmail.com>
2016-11-29 21:48:12 -0600

    launcher follows player around the boss launcher has a min and max range

--- Assets/Scripts/Boss/BossAttack/BasicRangedAttack.cs
+++ Assets/Scripts/Boss/BossAttack/BasicRangedAttack.cs
@@ -4,25 +4,31 @@ using System.Collections;
 public class BasicRangedAttack : MonoBehaviour {
 
 	private GameObject ProjectileLauncher; // This is the Projectile Launcher Object.
-	public GameObject Player; // This is the player that is being targeted.
+	private GameObject player; // This is the player that is being targeted.
+    private Vector3 futurePos;
+    private Quaternion angleToPlayer;
+
 	public float DistanceFromCenter; // This is how far away the Projectile Launcher is from the center of the boss.
-//	private GameObject Clone;
+    public float futureTime;
 
 	void Start () {
 		ProjectileLauncher = gameObject.transform.GetChild (0).gameObject; // This gets the child object which in this case is the projectile launcher.
+        ProjectileLauncher.transform.parent = gameObject.transform;
 	}
 
 	void Update () {
-		Vector3 PlayerPosition = Player.transform.position;
 
-		Quaternion AngleToPlayer = Quaternion.FromToRotation(Vector3.up, PlayerPosition - transform.position );
-//		Clone = Instantiate(ProjectileLauncher, transform.position, AngleToPlayer) as GameObject;
+        player = GameObject.FindGameObjectWithTag("Player");
 
-		ProjectileLauncher.transform.rotation = AngleToPlayer;
-		ProjectileLauncher.transform.parent = gameObject.transform;
-//		ProjectileLauncher.transform.Translate(new Vector3 (DistanceFromCenter, 0, 0));
+        //calculate thwere the player is headed too with futureTime
+        futurePos = new Vector3(player.transform.position.x + (player.GetComponent<Rigidbody2D>().velocity.x * futureTime), player.transform.position.y + (player.GetComponent<Rigidbody2D>().velocity.y * futureTime), player.transform.position.z);
 
-//		Destroy(Clone, 2);
-	}
+        Quaternion angleToPlayer = Quaternion.FromToRotation(Vector3.right, player.transform.position - transform.position );
+        Quaternion angleToFuture = Quaternion.FromToRotation(Vector3.right, futurePos - transform.position);
+
+        ProjectileLauncher.transform.position = transform.position;
+        ProjectileLauncher.transform.rotation = angleToPlayer;
+        ProjectileLauncher.transform.Translate(new Vector3(gameObject.transform.localScale.x / 2, 0f, 0f));
+    }
 
 }
--- Assets/Scripts/Boss/BossProjectile.cs
+++ Assets/Scripts/Boss/BossProjectile.cs
@@ -5,11 +5,13 @@ public class BossProjectile : MonoBehaviour {
 
 	public GameObject ProjectilePrefab;
 	public float Speed;
-
+    public float maxRange;
+    public float minRange;
 	public float Cooldown;
 	public float DestroyAfter;
 
 	private GameObject Clone;
+    private GameObject player;
 	private bool canFire;
 
 	void Start() {
@@ -18,14 +20,18 @@ public class BossProjectile : MonoBehaviour {
 
 
 	void Update () {
+        player = GameObject.FindGameObjectWithTag("Player");
 
-		if (canFire == true) {
-			canFire = false;
-			Clone = (Instantiate (ProjectilePrefab, transform.position, transform.rotation)) as GameObject; // Creates a new Rocket to fire forwards.
-			Clone.GetComponent<Rigidbody2D> ().AddRelativeForce (new Vector3 (0, Speed, 0)); // Launches the new Rocket forwards.
+        if (Vector3.Distance(player.transform.position, gameObject.transform.position) <= maxRange && Vector3.Distance(player.transform.position, gameObject.transform.position) >= minRange) {
+            if (canFire == true)
+            {
+                canFire = false;
+                Clone = (Instantiate(ProjectilePrefab, transform.position, transform.rotation)) as GameObject; // Creates a new Rocket to fire forwards.
+                Clone.GetComponent<Rigidbody2D>().AddRelativeForce(new Vector3(Speed, 0, 0)); // Launches the new Rocket forwards.
 
-			Destroy (Clone, DestroyAfter);
-			StartCoroutine ("RefreshShield");
+                Destroy(Clone, DestroyAfter);
+                StartCoroutine("RefreshShield");
+            }
 		}
 	}
 
