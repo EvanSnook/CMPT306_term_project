4f3ea47081284f0163bb2281bbef958176d7fb73
Evan <snovan@hotmail.com>
2016-11-30 00:23:41 -0600

    commented code and removed redundant script

--- Assets/Scripts/Boss/BossAttack/BasicRangedAttack.cs
+++ Assets/Scripts/Boss/BossAttack/BasicRangedAttack.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class BasicRangedAttack : MonoBehaviour {
 
-	private GameObject projectileLauncher; // This is the Projectile Launcher Object.
-	private GameObject player; // This is the player that is being targeted.
+	private GameObject projectileLauncher;
+	private GameObject player;
     private GameObject projectile;
     private Vector3 futurePos;
     private Quaternion angleToPlayer;
@@ -13,16 +13,20 @@ public class BasicRangedAttack : MonoBehaviour {
     public float futureTime;
     public GameObject ProjectilePrefab;
     public GameObject ProjectileLauncherPrefab;
-    public float Speed;
+    public float maxShootAngle;
+    public float projectileSpeed;
     public float maxRange;
     public float minRange;
     public float Cooldown;
     public float DestroyAfter;
 
     void Start () {
+
+        canFire = true;
+
+        //creating the projectile launcer and parenting the boss to it
         projectileLauncher = (Instantiate(ProjectileLauncherPrefab, transform.position, transform.rotation)) as GameObject;
         projectileLauncher.transform.parent = gameObject.transform;
-        canFire = true;
     }
 
 
@@ -30,28 +34,31 @@ public class BasicRangedAttack : MonoBehaviour {
 
         player = GameObject.FindGameObjectWithTag("Player");
 
+        //if the player is withing the max and min range
         if (Vector3.Distance(player.transform.position, gameObject.transform.position) <= maxRange && Vector3.Distance(player.transform.position, gameObject.transform.position) >= minRange)
         {
             if (canFire == true)
             {
                 canFire = false;
 
-                //calculate there the player is headed too with futureTime
+                //calculate where the player is headed too with futureTime
                 futurePos = new Vector3(player.transform.position.x + (player.GetComponent<Rigidbody2D>().velocity.x * futureTime), player.transform.position.y + (player.GetComponent<Rigidbody2D>().velocity.y * futureTime), player.transform.position.z);
 
+                //finding the current angle to the player and the angle to where the player is going
                 Quaternion angleToPlayer = Quaternion.FromToRotation(Vector3.right, player.transform.position - transform.position );
                 Quaternion angleToFuture = Quaternion.FromToRotation(Vector3.right, futurePos - transform.position);
 
+                //transforming the launcher to be nearby the player and around the outside of the boss
                 projectileLauncher.transform.position = transform.position;
                 projectileLauncher.transform.rotation = angleToPlayer;
                 projectileLauncher.transform.Translate(new Vector3(gameObject.transform.localScale.x / 2, 0f, 0f));
 
-                // Creates a new Rocket to fire
+                // Creates a new projectile to fire and rotating it towards where the player is going
                 projectile = (Instantiate(ProjectilePrefab, projectileLauncher.transform.position, projectileLauncher.transform.rotation)) as GameObject;
                 projectile.transform.rotation = angleToFuture;
 
-                // Launches the new Rocket forwards
-                projectile.GetComponent<Rigidbody2D>().AddRelativeForce(new Vector3(Speed, 0, 0)); 
+                // Launches the new projectile forwards
+                projectile.GetComponent<Rigidbody2D>().AddRelativeForce(new Vector3(projectileSpeed, 0, 0)); 
                 Destroy(projectile, DestroyAfter);
                 StartCoroutine("RefreshProjectile");
             }
