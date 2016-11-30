2243de8a7b8c8df16166df6a4b8180c7c3922920
Evan <snovan@hotmail.com>
2016-11-29 20:24:30 -0600

    boss propperly swings at you from a distance (balancing needed)

--- Assets/Scripts/Boss/BossAttack/BossMelee.cs
+++ Assets/Scripts/Boss/BossAttack/BossMelee.cs
@@ -9,14 +9,18 @@ public class BossMelee : MonoBehaviour {
     public float meleeDuration;
     public float meleeSpeed;
     public float startDistanceFromPlayer;
+    public float futureTime;
+    public float swingRadius;
 
     private GameObject player;
     private GameObject meleeAttack;
     private bool isAttacking;
     private bool onCooldown;
     private Quaternion angleToPlayer;
+    private Quaternion angleToFuture;
     private Vector3 startAngleFromPlayer;
     private float edgeAt45DegOut;
+    private Vector3 futurePos;
     private int attackDirection;
 
     // Use this for initialization
@@ -45,10 +49,10 @@ public class BossMelee : MonoBehaviour {
             {
                 isAttacking = true;
                 onCooldown = true;
-
+                
                 FindAngleOfAttack();
 
-                meleeAttack = Instantiate(meleePrefab, transform.position, angleToPlayer) as GameObject;
+                meleeAttack = Instantiate(meleePrefab, transform.position, angleToFuture) as GameObject;
 
                 //parent the object to the boss
                 meleeAttack.transform.parent = gameObject.transform;
@@ -61,7 +65,9 @@ public class BossMelee : MonoBehaviour {
         {
             if (attackDirection != 0)
             {
+                meleeAttack.transform.position = gameObject.transform.position;
                 meleeAttack.transform.Rotate(0f, 0f, meleeSpeed * attackDirection);
+                meleeAttack.transform.Translate(swingRadius, 0f, 0f);
             }
             else
             {
@@ -72,17 +78,15 @@ public class BossMelee : MonoBehaviour {
 
     private void FindAngleOfAttack()
     {
-        //top x works
-        //bottom x distance too far
-        //right y distance too close
-        //left y works?
+
+        futurePos = new Vector3(player.transform.position.x + (player.GetComponent<Rigidbody2D>().velocity.x * futureTime), player.transform.position.y + (player.GetComponent<Rigidbody2D>().velocity.y * futureTime), player.transform.position.z);
+
         if      ((player.transform.position.x > gameObject.transform.position.x + edgeAt45DegOut && player.GetComponent<Rigidbody2D>().velocity.y > 0) ||
                  (player.transform.position.x < gameObject.transform.position.x - edgeAt45DegOut && player.GetComponent<Rigidbody2D>().velocity.y < 0) ||
                  (player.transform.position.y > gameObject.transform.position.x + edgeAt45DegOut && player.GetComponent<Rigidbody2D>().velocity.x < 0) ||
                  (player.transform.position.y < gameObject.transform.position.x - edgeAt45DegOut && player.GetComponent<Rigidbody2D>().velocity.x > 0))
         {
-            startAngleFromPlayer = new Vector3(startDistanceFromPlayer, startDistanceFromPlayer, 0f);
-            angleToPlayer = Quaternion.FromToRotation(Vector3.zero, player.transform.position - transform.position + startAngleFromPlayer);
+            angleToFuture = Quaternion.FromToRotation(Vector3.right, futurePos - transform.position);
             attackDirection = -1;
         }
         else if ((player.transform.position.x > gameObject.transform.position.x + edgeAt45DegOut && player.GetComponent<Rigidbody2D>().velocity.y < 0) ||
@@ -90,8 +94,7 @@ public class BossMelee : MonoBehaviour {
                  (player.transform.position.y > gameObject.transform.position.x + edgeAt45DegOut && player.GetComponent<Rigidbody2D>().velocity.x > 0) ||
                  (player.transform.position.y < gameObject.transform.position.x - edgeAt45DegOut && player.GetComponent<Rigidbody2D>().velocity.x < 0))
         {
-            startAngleFromPlayer = new Vector3(-startDistanceFromPlayer, -startDistanceFromPlayer, 0f);
-            angleToPlayer = Quaternion.FromToRotation(-Vector3.right, player.transform.position - transform.position - startAngleFromPlayer);
+            angleToFuture = Quaternion.FromToRotation(Vector3.right, futurePos - transform.position);
             attackDirection = 1;
         }
 
@@ -99,6 +102,7 @@ public class BossMelee : MonoBehaviour {
             angleToPlayer = Quaternion.FromToRotation(Vector3.right, player.transform.position - transform.position);
             attackDirection = 0;
         }
+        
 
     }
     IEnumerator MeleeDuration()
