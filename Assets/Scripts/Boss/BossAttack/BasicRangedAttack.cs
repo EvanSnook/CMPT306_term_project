35f95749f61da4ba3505a014ef767f08f0d2f307
Tim <XxWolf9630xX@gmail.com>
2016-12-01 09:13:30 -0600

    Increased the Projectile Speed of Boss Ranged Attack from 1 (which is like the speed that glass flows) to 1000 (which is more resonable but am unsure if its too fast or too slow) Fixed Errors appearing from the BossBasicRanged Attack and BossBasic Melee Attack when the player dies.

--- Assets/Scripts/Boss/BossAttack/BasicRangedAttack.cs
+++ Assets/Scripts/Boss/BossAttack/BasicRangedAttack.cs
@@ -33,37 +33,35 @@ public class BasicRangedAttack : MonoBehaviour {
 	void Update () {
 		if (player == null) {
 			player = GameObject.FindGameObjectWithTag ("Player");
+		} else {
+			//if the player is withing the max and min range
+			if (Vector3.Distance (player.transform.position, gameObject.transform.position) <= maxRange && Vector3.Distance (player.transform.position, gameObject.transform.position) >= minRange) {
+				if (canFire == true) {
+					canFire = false;
+
+					//calculate where the player is headed too with futureTime
+					futurePos = new Vector3 (player.transform.position.x + (player.GetComponent<Rigidbody2D> ().velocity.x * futureTime), player.transform.position.y + (player.GetComponent<Rigidbody2D> ().velocity.y * futureTime), player.transform.position.z);
+
+					//finding the current angle to the player and the angle to where the player is going
+					Quaternion angleToPlayer = Quaternion.FromToRotation (Vector3.right, player.transform.position - transform.position);
+					Quaternion angleToFuture = Quaternion.FromToRotation (Vector3.right, futurePos - transform.position);
+
+					//transforming the launcher to be nearby the player and around the outside of the boss
+					projectileLauncher.transform.position = transform.position;
+					projectileLauncher.transform.rotation = angleToPlayer;
+					projectileLauncher.transform.Translate (new Vector3 (gameObject.transform.localScale.x / 2, 0f, 0f));
+
+					// Creates a new projectile to fire and rotating it towards where the player is going
+					projectile = (Instantiate (ProjectilePrefab, projectileLauncher.transform.position, projectileLauncher.transform.rotation)) as GameObject;
+					projectile.transform.rotation = angleToFuture;
+					projectile.GetComponent<DMG> ().Owner = this.gameObject;
+					// Launches the new projectile forwards
+					projectile.GetComponent<Rigidbody2D> ().AddRelativeForce (new Vector3 (projectileSpeed, 0, 0)); 
+					Destroy (projectile, DestroyAfter);
+					StartCoroutine ("RefreshProjectile");
+				}
+			}
 		}
-
-        //if the player is withing the max and min range
-        if (Vector3.Distance(player.transform.position, gameObject.transform.position) <= maxRange && Vector3.Distance(player.transform.position, gameObject.transform.position) >= minRange)
-        {
-            if (canFire == true)
-            {
-                canFire = false;
-
-                //calculate where the player is headed too with futureTime
-                futurePos = new Vector3(player.transform.position.x + (player.GetComponent<Rigidbody2D>().velocity.x * futureTime), player.transform.position.y + (player.GetComponent<Rigidbody2D>().velocity.y * futureTime), player.transform.position.z);
-
-                //finding the current angle to the player and the angle to where the player is going
-                Quaternion angleToPlayer = Quaternion.FromToRotation(Vector3.right, player.transform.position - transform.position );
-                Quaternion angleToFuture = Quaternion.FromToRotation(Vector3.right, futurePos - transform.position);
-
-                //transforming the launcher to be nearby the player and around the outside of the boss
-                projectileLauncher.transform.position = transform.position;
-                projectileLauncher.transform.rotation = angleToPlayer;
-                projectileLauncher.transform.Translate(new Vector3(gameObject.transform.localScale.x / 2, 0f, 0f));
-
-                // Creates a new projectile to fire and rotating it towards where the player is going
-                projectile = (Instantiate(ProjectilePrefab, projectileLauncher.transform.position, projectileLauncher.transform.rotation)) as GameObject;
-                projectile.transform.rotation = angleToFuture;
-				projectile.GetComponent<DMG> ().Owner = this.gameObject;
-                // Launches the new projectile forwards
-                projectile.GetComponent<Rigidbody2D>().AddRelativeForce(new Vector3(projectileSpeed, 0, 0)); 
-                Destroy(projectile, DestroyAfter);
-                StartCoroutine("RefreshProjectile");
-            }
-        }
     }
 
     IEnumerator RefreshProjectile()
