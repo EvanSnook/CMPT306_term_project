be4fe630c66d33c55e91f447e3e1982295698e4b
Tim <XxWolf9630xX@gmail.com>
2016-12-06 17:21:12 -0600

    Added Comments to BossDeath Script. I DON'T THINK THAT THIS SCRIPT IS USED ANYMORE AND NEEDS TO BE DELETED.

--- Assets/Scripts/BossDeath.cs
+++ Assets/Scripts/BossDeath.cs
@@ -3,16 +3,17 @@ using System.Collections;
 
 public class BossDeath : MonoBehaviour {
 
-	public GameObject SceneController;
-	public GameObject SavedData;
+	public GameObject SceneController; // This will hold a reference to the SceneController Object.
+	public GameObject SavedData; // This will hold a reference to the SavedData Object.
 
 	void Start () {
-		SceneController = GameObject.Find("SceneControllerObject");
-		SavedData = GameObject.Find ("SavedData");
+		SceneController = GameObject.Find("SceneControllerObject"); // Get a Reference to the scene controller Object.
+		SavedData = GameObject.Find ("SavedData"); // Get a Referecne to the SavedData Object.
 	}
 
+    // This is called when the boss Dies.
 	public void BossDied() {
-		SavedData.GetComponent<SavedData>().SendMessage("SaveData");
-		SceneController.SendMessage("ChangeScene");
+		SavedData.GetComponent<SavedData>().SendMessage("SaveData"); // If the boss has Died send a message to save data.
+		SceneController.SendMessage("ChangeScene"); // If the boss has died Sence a message to change the scene.
 	}
 }
