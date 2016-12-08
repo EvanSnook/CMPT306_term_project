3450d99506249ebdffbe558d6ff9b5b468ae396c
Justin <jlehmann96@gmail.com>
2016-12-08 07:51:51 -0600

    Health bar is functional

--- Assets/Scripts/GameComponenets/PlayerGUIManager.cs
+++ Assets/Scripts/GameComponenets/PlayerGUIManager.cs
@@ -9,12 +9,27 @@ public class PlayerGUIManager : MonoBehaviour {
 
 	public GameObject HealthNumberGUI; // This is the Element that Holds how much Health the player has.
 	public GameObject TimerGUI; // This is the Element that Holds how much time is remaining.
+	public GameObject HealthBarGUI;
+	public GameObject MeleeCooldownGUI;
+	public GameObject RangedCooldownGUI;
+	public GameObject QCooldownGUI;
+	public GameObject ECooldownGUI;
 
+	private Image HealthBar;
+	private Image MeleeCooldown;
+	private Image RangedCooldown;
+	private Image QCooldown;
+	private Image ECooldown;
 	private Text HealthNumber; // This is the Text for the TextGUI that holds the HealthNumber.
 	private Text Timer; // This is the Text for the TextGUI that holds the Timer.
 
 	void Start () {
 		HealthNumber = HealthNumberGUI.GetComponent<Text> (); // This is the text of the GUI object.
+		HealthBar = HealthBarGUI.GetComponent<Image> ();
+		MeleeCooldown = MeleeCooldownGUI.GetComponent<Image> ();
+		RangedCooldown = RangedCooldownGUI.GetComponent<Image> ();
+		QCooldown = QCooldownGUI.GetComponent<Image> ();
+		ECooldown = ECooldownGUI.GetComponent<Image> ();
 		Timer = TimerGUI.GetComponent<Text>(); // This is the text of the GUI object for the Timer.
 	}
 
@@ -23,6 +38,7 @@ public class PlayerGUIManager : MonoBehaviour {
 		if (Player == null) {
 			Player = GameObject.FindGameObjectWithTag ("Player"); // This gets a reference to the player if this has not be found.
 		} else {
+			HealthBar.fillAmount = Player.GetComponent<Health> ().HealthPoints / 100f;
 			HealthNumber.text = "Health: " + Player.GetComponent<Health> ().HealthPoints; // This gets the current Health Component and sets the GUI object to it.
 			Timer.text = "Time Remaining: " + TimerObject.GetComponent<GameTimer> ().GameCountdown.ToString("N0"); // This gets the current TimeRemaining and sets the GUI object to it.
 		}
