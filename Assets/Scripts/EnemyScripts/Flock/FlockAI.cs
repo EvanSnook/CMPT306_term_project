7a2eaf2689ad1fde2436769a590bb35833874e9a
Moniloth\Jlehm <jlehmann96@gmail.com>
2016-12-11 07:44:02 -0600

    Added new testscene for "Flock", tiny triangles that fly around as flocks of birds and persue the player. Lags like hell with too many of them though

---
+++ Assets/Scripts/EnemyScripts/Flock/FlockAI.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class FlockAI : MonoBehaviour {
+
+	public FlockSightManager cohesionRange;
+	public FlockSightManager alignmentRange;
+	public FlockSightManager separationRange;
+	public float cohesionFactor;
+	public float alignmentFactor;
+	public float separationFactor;
+	public float inertiaFactor;
+	public float chaseFactor;
+	public float speed;
+	public float headingInterval;
+
+	private Rigidbody2D rb;
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		rb.velocity = transform.up * speed;
+		InvokeRepeating("Realign", 0, headingInterval);
+	}
+
+	void FixedUpdate() {
+		if (player == null) {
+			player = GameObject.FindWithTag ("Player");
+		}
+	}
+
+	// Update is called once per frame
+	void Realign () {
+		Vector2 velocity = rb.velocity;
+		velocity += (inertiaFactor * rb.velocity) + (alignmentFactor * calcAlignment()) + (cohesionFactor * calcCohesion()) + (separationFactor * calcSeparation()) + (chaseFactor * chasePlayer());
+		velocity.Normalize();
+		rb.velocity = velocity * speed;
+		transform.rotation = Quaternion.AngleAxis(Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg - 90, Vector3.forward);
+	}
+
+	Vector2 calcCohesion() {
+		Vector2 result = new Vector2(0f, 0f);
+		if (cohesionRange.otherFlock.Count == 0) {
+			return result;
+		}
+		foreach (GameObject other in cohesionRange.otherFlock) {
+			result.x += other.GetComponent<Rigidbody2D>().velocity.x;
+			result.y += other.GetComponent<Rigidbody2D>().velocity.y;
+		}
+		result.x /= cohesionRange.otherFlock.Count;
+		result.y /= cohesionRange.otherFlock.Count;
+		result.Normalize();
+		return result;
+	}
+
+	Vector2 calcAlignment() {
+		Vector2 result = new Vector2(0f, 0f);
+		if (alignmentRange.otherFlock.Count == 0) {
+			return result;
+		}
+		foreach (GameObject other in alignmentRange.otherFlock) {
+			result.x += other.transform.position.x;
+			result.y += other.transform.position.y;
+		}
+		result.x /= alignmentRange.otherFlock.Count;
+		result.y /= alignmentRange.otherFlock.Count;
+		result = new Vector2(result.x - transform.position.x, result.y - transform.position.y);
+		result.Normalize();
+		return result;
+	}
+
+	Vector2 calcSeparation() {
+		Vector2 result = new Vector2(0f, 0f);
+		if (separationRange.otherFlock.Count == 0) {
+			return result;
+		}
+		foreach (GameObject other in separationRange.otherFlock) {
+			result.x += other.transform.position.x - transform.position.x;
+			result.y += other.transform.position.y - transform.position.y;
+		}
+		result.x /= separationRange.otherFlock.Count;
+		result.y /= separationRange.otherFlock.Count;
+		result.x *= -1;
+		result.y *= -1;
+		//result = new Vector2(result.x - transform.position.x, result.y - transform.position.y);
+		result.Normalize();
+		return result;
+	}
+
+	Vector2 chasePlayer() {
+		Vector2 result = new Vector2(0f, 0f);
+		if (player != null) {
+			result.x = player.transform.position.x - transform.position.x;
+			result.y = player.transform.position.y - transform.position.y;
+		}
+		result.Normalize();
+		return result;
+	}
+}
---
+++ Assets/Scripts/EnemyScripts/Flock/FlockSightManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FlockSightManager : MonoBehaviour {
+
+	public List<GameObject> otherFlock = new List<GameObject>();
+
+	private GameObject parent;
+
+	void Start () {
+		parent = transform.parent.gameObject;
+	}
+
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject != parent && other.gameObject.GetComponent("FlockAI") != null) {
+			otherFlock.Add(other.gameObject);
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if (other.gameObject != parent && other.gameObject.GetComponent("FlockAI") != null) {
+		otherFlock.Remove(other.gameObject);
+		}
+	}
+}
